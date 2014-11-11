23d219c06fc8de0ba5c8c1f5ad9df1a5a101ddbe
ATalaba <andrei_talaba@yahoo.com>
2014-11-11 17:47:34 -0500

    Added name adder thingy on first panel, and made the rest more material designy

--- animation.js
+++ animation.js
@@ -1,13 +1,37 @@
+$(document).ready(function() {
+    $(".inputSubmit").click(function(e) {
+        e.preventDefault();
+        document.getElementById('command').blur()
+        addClose()})
+    $('#command').blur(function() {
+        addClose()})
+                .focus(function() {
+        remClose()})
+    $("#x").click(function() {
+        remClose()
+        document.getElementById('command').value = '';
+        document.getElementById('command').focus();});
+});
+
+function addClose() {
+    document.getElementById('x').style.visibility = 'visible';
+    $('#command').attr('placeholder', '');
+}
+
+function remClose() {
+    document.getElementById('x').style.visibility = 'hidden';
+}
+
 function header() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 10,
             header = document.getElementById("head");
         if (distanceY > shrinkOn) {
-            header.style.backgroundColor = "rgb(0, 19, 178)";
+            header.style.backgroundColor = "rgb(88, 185, 231)";
             header.style.opacity = "0.9";
         } else {
-            if (header.style.backgroundColor == "rgb(0, 19, 178)") {
+            if (header.style.backgroundColor == "rgb(88, 185, 231)") {
                 header.style.backgroundColor = '';
                 header.style.opacity= '1';
             }
