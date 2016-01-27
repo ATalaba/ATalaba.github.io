582d99415f176dcd68f060049f7d074ca1d5263f
ATalaba <at694@cornell.edu>
2016-01-26 21:28:07 -0500

    Removed splash screen and unnecessary panels

--- animation.js
+++ animation.js
@@ -1,49 +1,3 @@
-$(document).ready(function() {
-    $(".inputSubmit").on('click', function(e) {
-        if (e.preventDefault) {e.preventDefault();}
-        else {e.returnValue = False}
-        document.getElementById('command').blur()
-        addClose(e)})
-    $('#command').blur(function() {
-        document.getElementById('x').style.visibility = 'visible';})
-                .focus(function() {
-        remClose()})
-    $("#x").click(function() {
-        remClose()
-        document.getElementById('command').value = '';
-        document.getElementById('command').focus();});
-});
-
-function addClose(e) {
-    $('#command').attr('placeholder', '');
-    $('hmtl, body').animate({
-        scrollTop: $('.about').offset().top},
-        'slow', function() {});
-    document.getElementById('yourname').innerHTML = $(".inputCommand").val()
-}
-
-function remClose() {
-    document.getElementById('x').style.visibility = 'hidden';
-}
-
-function header() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 10,
-            header = document.getElementById("head");
-        if (distanceY > shrinkOn) {
-            header.style.backgroundColor = "rgb(88, 185, 231)";
-            header.style.opacity = "0.9";
-        } else {
-            if (header.style.backgroundColor == "rgb(88, 185, 231)") {
-                header.style.backgroundColor = '';
-                header.style.opacity= '1';
-            }
-        }
-    });
-}
-window.onload = header();
-
 $('.navli, .name').click(function(e) {
     $('hmtl, body').animate({
         scrollTop: $('.' + $(this).attr('id')).offset().top},
@@ -62,7 +16,7 @@ $('#all').mouseenter(function() {
 $('#web').mouseenter(function() {
     $('#html').show(300);
     $('#js').show(300);
-    $('#python').hide(300)
+    $('#python').show(300)
     $('#jq').show(300);
     $('#njs').show(300);
     $('#jav').hide(300);
